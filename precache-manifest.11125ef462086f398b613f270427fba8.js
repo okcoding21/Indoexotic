4ae62f827e3bca75bff9f6fc55ebdad98903cadb
okcoding21 <rohit.asawa21@gmail.com>
2020-08-15 14:20:13 +0530

    d

---
+++ precache-manifest.11125ef462086f398b613f270427fba8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02edfbd97089459be582930f9fa66a93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2655e1120d1c475bbaef",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "816675c461c8d585c9e8",
+    "url": "/static/js/2.5243668c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5243668c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2655e1120d1c475bbaef",
+    "url": "/static/js/main.4cdbeb34.chunk.js"
+  },
+  {
+    "revision": "31f49e15a394c5609b4a",
+    "url": "/static/js/runtime-main.476e1fa5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
