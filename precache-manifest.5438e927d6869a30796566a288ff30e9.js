fb737498389fa0d2a81fe6dc4e87ff717038df3a
okcoding21 <rohit.asawa21@gmail.com>
2020-08-15 14:22:24 +0530

    d

---
+++ precache-manifest.5438e927d6869a30796566a288ff30e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff5e5e027bc785984ddffc8969e1aaf4",
+    "url": "/Indoexotic/index.html"
+  },
+  {
+    "revision": "65bfaa8abb10a38c97be",
+    "url": "/Indoexotic/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "816675c461c8d585c9e8",
+    "url": "/Indoexotic/static/js/2.5243668c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Indoexotic/static/js/2.5243668c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65bfaa8abb10a38c97be",
+    "url": "/Indoexotic/static/js/main.3dc2971d.chunk.js"
+  },
+  {
+    "revision": "1e134769a1c585b7a3a5",
+    "url": "/Indoexotic/static/js/runtime-main.223652c6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Indoexotic/static/media/logo.5d5d9eef.svg"
+  }
+]);
